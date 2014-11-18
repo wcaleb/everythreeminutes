7485ebf1b4bd9a55ca7404463c4052c05c49d59e
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2014-11-17 20:43:22 -0600

    Add more blank URLs

--- everythree.py
+++ everythree.py
@@ -34,7 +34,7 @@ urls =  ['http://books.google.com/books?id=TUtFgWOISxMC&lpg=PA124&ots=JkLLPw4h9o
          'http://books.google.com/books?id=-dbFUlQvcRYC&lpg=PP11&ots=rrAzJ-6IUU&pg=PA347#v=onepage&q=minutes&f=false',
          'http://wcm1.web.rice.edu/slave-sales-on-twitter.html',
          'http://hitchcock.itc.virginia.edu/Slavery/details.php?categorynum=6&theRecord=42',
-         '', '', ''] 
+         '', '', '', '', ''] 
 
 # Get a random item from the times and acts lists; join with delimiters
 snippets = [random.choice(x) for x in [times, acts]]
